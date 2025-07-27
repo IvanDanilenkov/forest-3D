da4db9fd286087dd262ceaf4cf32a25bcd0624f8
Ivan Danilenkov <danilich071@gmail.com>
2025-07-26 22:46:48 -0400

    refactor: Added button style

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 document.addEventListener('mousemove', evt => {
   Object.assign(document.documentElement, {
     style: `
-      --move-x: ${(evt.clientX - window.innerWidth / 2) * -0.01}deg;
-      --move-y: ${(evt.clientY - window.innerWidth / 2) * -0.01}deg;
+      --move-x: ${(evt.clientX - window.innerWidth / 2) * -0.005}deg;
+      --move-y: ${(evt.clientY - window.innerWidth / 2) * -0.005}deg;
     `
   });
 })
